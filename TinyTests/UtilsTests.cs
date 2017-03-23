393608e88cbfb5421092906a34633806088c28d5
mr.x <chup.zz.zz@gmail.com>
2017-03-23 12:03:04 +0200

    Worked and tested local entity

---
+++ TinyTests/UtilsTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tiny;
+
+namespace TinyTests
+{
+    [TestClass]
+    public class UtilsTests
+    {
+        private const uint PrecalculatedCrc32Valuefor0X00110011 = 0x561036A9;
+
+        [TestMethod]
+        public void Crc32()
+        {
+            var crccalc = new Crc32();
+            var calculatedvalue = crccalc.ComputeChecksum(BitConverter.GetBytes(0x00110011));
+
+            Assert.IsTrue(calculatedvalue == PrecalculatedCrc32Valuefor0X00110011);
+
+            var precalculatedvalueasarray = BitConverter.GetBytes(PrecalculatedCrc32Valuefor0X00110011);
+            var calculatedvalueasbytearray = crccalc.ComputeChecksumBytes(BitConverter.GetBytes(0x00110011));
+
+            Assert.IsTrue(precalculatedvalueasarray == calculatedvalueasbytearray);
+
+        }
+    }
+}
